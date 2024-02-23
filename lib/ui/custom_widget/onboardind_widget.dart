78d9ab3f27e3b517fce23a85a0aa4f3115587e0f
Ahmed Salah Elsharkawy <96499153+Ah-sala7@users.noreply.github.com>
2024-02-23 10:45:21 +0200

    Revert "Login provider repository pattern"

--- lib/ui/custom_widget/onboardind_widget.dart
+++ lib/ui/custom_widget/onboardind_widget.dart
@@ -3,14 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 class OnboardingWidget extends StatelessWidget {
   String imagePath;
-  String header;
-String content;
-  ElevatedButton? button;
+  String? header;
+String? content;
   OnboardingWidget({
     required this.imagePath,
     this.header = "",
-    this.content = "",
-    this.button  ,
+    this.content = ""
   });
   Widget build(BuildContext context) {
     var hight = MediaQuery.of(context).size.height;
@@ -29,16 +27,17 @@ String content;
           Stack(
             alignment: AlignmentDirectional.center,
             children: [
-              Expanded(child: Image.asset("assets/images/background_onboarding.png", width: double.infinity, fit: BoxFit.fitWidth)),
+              Expanded(
+                  child: Image.asset("assets/images/background_onboarding.png",
+                      width: double.infinity, fit: BoxFit.fitWidth)),
               Positioned(
                 child: Text(
-                  header??"",
+                  header!,
                   style: TextStyle(
                       color: Colors.white,
-                      fontSize: 29,
+                      fontSize: 30,
                       fontFamily: "Poppins",
-
-                      fontWeight: FontWeight.w700),
+                      fontWeight: FontWeight.w500),
                   textAlign: TextAlign.center,
                 ),
                 height: hight * .3,
@@ -46,28 +45,17 @@ String content;
               ),
               Positioned(
                 child: Text(
-                  content??"",
+                  content!,
                   style: TextStyle(
                       color: Colors.white,
-                      fontSize: 14,
+                      fontSize: 12,
                       fontFamily: "Poppins",
                       fontWeight: FontWeight.w500),
                   textAlign: TextAlign.center
                 ),
-                height: hight * .07,
-                width: width * .8,
+                height: hight * .04,
+                width: width * .5,
               ),
-
-                if(button != null)
-                SizedBox(
-                  child: button,
-                  width: width * .8,
-                  height: hight * .08,
-                )
-            ,
-
-
-
             ],
           )
         ],
